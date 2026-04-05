22bf529cf2f141bed0af6ebbc2cb1a2c99ad5235
Sachin Sharma <sachinsharma@Sachins-MacBook-Air.local>
2026-04-05 10:54:10 +0530

    adding feature

--- src/Layout/MainContent.js
+++ src/Layout/MainContent.js
@@ -1,9 +1,46 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import NewsCard from '../components/NewsCard'
 
 const MainContent = () => {
+    const [news, setNews] = useState([])
+    const [error, setError] = useState("")
+    const [isLoading, setIsLoading] = useState(true)
+
+    const API_KEY = process.env.REACT_APP_NEWS_API_KEY;
+    const BASE_URL = process.env.REACT_APP_NEWS_URL;
+    
+    console.log("API_KEY:", API_KEY);
+    console.log("BASE_URL:", BASE_URL);
+
+    const getData = async () => {
+        try {
+          const res = await fetch(
+            `${BASE_URL}?apikey=${API_KEY}&country=in&language=en`
+          );
+          const data = await res.json();
+          setNews(data.results);
+        } catch (err) {
+          console.log(err);
+        }
+        finally{
+            setIsLoading(false)
+        }
+      };
+    useEffect(()=>{
+        getData();
+    },[])
+    console.log(news)
   return (
     <div>
-     <h1 className="text-2xl font-bold">Welcome 🚀</h1>
+        {news.map((data)=>(
+            // <li key={data.article_id}>{data.title}</li>
+              <NewsCard id={data.article_id}
+              title={data.title}
+               />
+        ))}
+          
+     
+    
     </div>
   )
 }
---
+++ src/components/NewsCard.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const NewsCard = ({id, title}) => {
+  return (
+    <div>
+        <h1>{id}</h1>
+        <h2>{title}</h2>
+      
+    </div>
+  )
+}
+
+export default NewsCard
